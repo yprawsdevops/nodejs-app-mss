0004a878926bb312036916db00adabe0cea1dfae
Srinu-web <112843932+Srinu-web@users.noreply.github.com>
2022-10-24 19:05:10 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.141.4:6304/',
+  serverUrl: 'http://3.110.90.198:6304/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
